10ba6a8ae3c6439a3dcb9685f471f7426d05090d
theastrobhaiya-debug <theastrobhaiya@gmail.com>
2026-09-02 00:58:51 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <div className="footer-bottom">
-        © {new Date().getFullYear()} KaalDarpan · Powered by Mauksh
+        © {new Date().getFullYear()} GrahGanit · Powered by Mauksh
       </div>
     </footer>
   );
