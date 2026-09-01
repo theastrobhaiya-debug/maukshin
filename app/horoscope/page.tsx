4a5ef4ad757b1727232631d00912130a1dc0cf67
theastrobhaiya-debug <theastrobhaiya@gmail.com>
2026-09-02 01:01:45 +0530

    Update page.tsx

--- app/horoscope/page.tsx
+++ app/horoscope/page.tsx
@@ -125,7 +125,7 @@ export default function HoroscopePage() {
           <div className="kd-brand-row">
 
             <div className="kd-brand">
-              KAALDARPAN
+              GRAHGANIT
             </div>
 
             <div className="kd-brand-line" />
@@ -377,7 +377,7 @@ export default function HoroscopePage() {
           <div>
 
             <div className="kd-footer-brand">
-              KAALDARPAN
+              GRAHGANIT
             </div>
 
             <p>
